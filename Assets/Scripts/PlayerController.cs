5eb7c834fa246162bbb22ffbd26f174a34b7e2c0
chunkind <chunkind@naver.com>
2021-10-22 22:20:01 +0900

    slerp를 이용하여 스르르 움직임 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,22 +30,22 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
             //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
         }   
         if (Input.GetKey(KeyCode.S))
         {
-            transform.rotation = Quaternion.LookRotation(Vector3.back);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
             //transform.Translate(Vector3.back * Time.deltaTime * _speed);
         }   
         if (Input.GetKey(KeyCode.A))
         {
-            transform.rotation = Quaternion.LookRotation(Vector3.left);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
             //transform.Translate(Vector3.left * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.rotation = Quaternion.LookRotation(Vector3.right);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
             //transform.Translate(Vector3.right * Time.deltaTime * _speed);
         }
             
