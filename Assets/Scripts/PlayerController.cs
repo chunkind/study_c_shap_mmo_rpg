aa1b143c5efe980afd16889e0d94d739a4f9a3b6
chunkind <chunkind@naver.com>
2021-10-22 22:25:21 +0900

    이동시 유턴하듯 튀는현상 제거

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,22 +25,22 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
         }   
         if (Input.GetKey(KeyCode.S))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
         }   
         if (Input.GetKey(KeyCode.A))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
         }
             
     }
