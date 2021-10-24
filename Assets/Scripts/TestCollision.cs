fb58af68a2ea678485416062eaeee86c6104f8ae
chunkind <chunkind@naver.com>
2021-10-24 11:10:41 +0900

    raycast 카메라가 2d로 표현하는 원리

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -38,5 +38,36 @@ public class TestCollision : MonoBehaviour
         // Debug.Log(Input.mousePosition); //Screen ÁÂÇ¥ (ÇÈ¼¿ÁÂÇ¥)
         // Camera.main.ScreenToViewportPoint(Input.mousePosition); //viewport ÁÂÇ¥(ÇÈ¼¿ºñÀ² ÁÂÇ¥)
 
+        //¿ø¸®¹öÀü
+        //if (Input.GetMouseButtonDown(0))
+        //{ //0¹ø :  ¿ÞÂÊ¸¶¿ì½º
+        //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        //    Vector3 dir = mousePos - Camera.main.transform.position;
+        //    dir = dir.normalized;
+
+        //    Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
+
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100.0f))
+        //    {
+        //        Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+        //    }
+        //}
+
+        //°£·«ÇÑ¹öÀü
+        if (Input.GetMouseButtonDown(0))
+        { //0¹ø :  ¿ÞÂÊ¸¶¿ì½º
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
+        }
+
     }
 }
